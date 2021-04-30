dd05c8c6a63f85adbb57399415465b96e7f82270
Alex Van Gils <alexvangils@gmail.com>
2021-04-30 14:57:02 -0400

    add scripts from unity

---
+++ unity-scripts/Scripts/DetectCollisions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void pickSampleFromUnity(int x);    
+
+    [DllImport("__Internal")]
+    private static extern void setParamWithFloat(string paramName, float value); 
+
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 20;
+    public GameObject targetBox;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Randomizer();
+        SpawnBox();
+        // Destroy the target you hit
+        Destroy(other.gameObject);
+    }
+
+    void Randomizer()
+    {
+        int val = Random.Range(0, 100);
+        int sampleIndex = Random.Range(0, 3);
+
+        // this number, which will be 0, 1, or 2, will set which sample should be triggered
+        pickSampleFromUnity(sampleIndex);
+
+        // update a param called "mod" in our device
+        setParamWithFloat("mod", val);
+    }
+    void SpawnBox()
+    {
+        // randomly generate spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 1.5f, Random.Range(-spawnRangeZ, spawnRangeZ));
+
+        Instantiate(targetBox, spawnPos, targetBox.transform.rotation);
+    }
+}
---
+++ unity-scripts/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void setParamWithFloat(string paramName, float value);
+
+    public float horizontalInput;
+    public float forwardInput;
+    public float speed = 10.0f;
+    public float xRange = 19.0f;
+    public float zRange = 19.0f;
+    private float horizLoc;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //wrap the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+
+        //move character left and right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //move character forward and back
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+
+        // send horizontal position to js
+        horizLoc = transform.position.x;
+        setParamWithFloat("modTwo", horizLoc);
+    }
+}
